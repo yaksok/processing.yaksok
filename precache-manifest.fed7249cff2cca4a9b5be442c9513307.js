92ebe4db0a6b4470482698e1889819b0a91e3026
JongChan Choi <jong@chan.moe>
2020-02-10 03:09:48 +0900

    Updates

---
+++ precache-manifest.fed7249cff2cca4a9b5be442c9513307.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b74f3fc362d02ec57f9e979b9f738096",
+    "url": "/index.html"
+  },
+  {
+    "revision": "6eac00a4358e3034db56",
+    "url": "/static/css/main.ccb05089.chunk.css"
+  },
+  {
+    "revision": "a005ebb36566d8a734c6",
+    "url": "/static/js/2.e5c250cb.chunk.js"
+  },
+  {
+    "revision": "d705cb622423d72c5defbf368ca70dcc",
+    "url": "/static/js/2.e5c250cb.chunk.js.LICENSE"
+  },
+  {
+    "revision": "6eac00a4358e3034db56",
+    "url": "/static/js/main.a2b38a37.chunk.js"
+  },
+  {
+    "revision": "1c71ebcc155335a332fc",
+    "url": "/static/js/runtime-main.8bdcc34d.js"
+  }
+]);
